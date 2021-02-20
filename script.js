7204000181e8ec814901babcdb76fc66242994b9
CreatrPages <creatrpages@gmail.com>
2021-02-20 08:30:14 +0530

    Update

--- script.js
+++ script.js
@@ -57,14 +57,3 @@ btn.forEach(el => {
 
 
 // Form Select
-
-
-
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
